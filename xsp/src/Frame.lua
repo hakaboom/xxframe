681ccbe1d88afe6c5ab0766d5762bbb0b67e8c1b
hakaboom <270704881@qq.com>
2019-03-22 23:07:23 +0800

    增加File的api

--- xsp/src/Frame.lua
+++ xsp/src/Frame.lua
@@ -592,23 +592,30 @@ File={
 }
 function File:new(filename,Path)--File:new("userconfig","自定义路径")
 	local o={
-		PublicPath=xmod.getPublicPath(),	--引擎公共文件夹目录路径
-		PrivatePath=xmod.getPrivatePath,	--脚本私有文件夹目录路径
-		Filename=filename,					--文件名
-		FilePath="",
+		Filename =filename,					--文件名
+		FilePath ='',
+		Path = '',
 	}
-	if Path=="public" then 
+	Path = Path or "private"
+	if Path=='public' then 
 		o.FilePath=xmod.resolvePath('[public]'..filename)
-	elseif Path=="private" then
+		o.Path = xmod.getPublicPath()
+	elseif Path=='private' then
 		o.FilePath=xmod.resolvePath('[private]'..filename)
-	elseif Path=="Path" then
-		o.FilePath=filename	--选择Path需要自己补全路径
-	else
-		print("请选择public或private")
-		xmod.exit()
+		o.Path = xmod.getPrivatePath()
+	elseif Path=='Path' then
+		o.FilePath=Path .. filename	--选择Path需要自己补全路径
+		o.Path = Path
 	end
 	setmetatable(o,{ __index = self})
 	return o
+end
+function File:Write(data)
+	local file = io.open(self.FilePath,'r+')
+	assert(file,"path of the file don't exist or can't open")
+	file:write(data)
+	file:flush()
+	file:close()
 end
 function File:WriteNewByJson(tbl)--以Json格式写入文件tbl={["xxxxx"]="123",["aaaaaa"]=true}
 	local json=require("cjson")
@@ -650,17 +657,21 @@ function File:ClearFile()--清除当前文件内容
 	file:flush()
 	file:close()
 end
-function File:check(data)--检查是否有文件没有则会创建,并且写入data里的内容
-	if io.open(self.FilePath,"r")==nil then
+function File:check(newBuild)--检测文件是否存在
+	if io.open(self.FilePath,'r')==nil then
 		print("没有"..self.Filename.."这个文件")
-		io.open(self.FilePath,"w"):close()
-		self:WriteNewByJson(data)
-	else
-		local file=io.open(self.FilePath,"r") 
-		if file:seek("end")==0 then
-			self:WriteNewByJson(data)
+		if newBuild==true then
+			io.open(self.FilePath,"w"):close()
 		end
+		return false
 	end
+	return true
+end
+function File:rename(newName)--重设文件名
+	os.rename(self.FilePath,(self.Path..newName))
+end
+function File:remove()--删除文件
+	os.remove(self.FilePath)
 end
 function File:printPath()--打印当前文件路径
 	printf("路径为 : %s",self.FilePath)
